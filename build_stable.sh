7f38ab8a91edec7f68ad27467f761991e89247e8
reigadegr <2722688642@qq.com>
2025-08-24 06:37:15 +0800

    fix: 修复build_stable.sh

--- build_stable.sh
+++ build_stable.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 export RUSTFLAGS="
-    -C default-linker-libraries \
     -C relocation-model=static \
     -C symbol-mangling-version=v0 \
     -C llvm-args=-fp-contract=off \
     -C llvm-args=-enable-misched \
     -C llvm-args=-enable-post-misched \
     -C llvm-args=-enable-dfa-jump-thread \
+    -C link-args=-Wl,--sort-section=alignment \
     -C link-args=-Wl,-O3,--gc-sections,--as-needed \
     -C link-args=-Wl,-z,relro,-z,now,-x,-z,noexecstack,-s,--strip-all
-" 
+"
 
 cargo update
 
