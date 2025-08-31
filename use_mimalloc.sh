8b2c2fc4e681ab35975dcecfd02fbfc121aa64cf
reigadegr <2722688642@qq.com>
2025-08-31 12:01:45 +0800

    fix: 多mallocator冲突

--- use_mimalloc.sh
+++ use_mimalloc.sh
@@ -1,11 +1,16 @@
 for i in $(grep -rn "fn main"); do
     i="$(echo $i | cut -d ':' -f1)"
-    
+    [ -f "$i" ] || continue
+
+    sed -i '/Jemalloc/d' $i || true
+    sed -i '/global_allocator/d' $i || true
+    sed -i '/MiMalloc/d' $i || true
+    sed -i '/mimalloc/d' $i || true
+    sed -i '/jemalloc/d' $i || true
     if [ -n "$(echo $i | grep 'use_mimalloc.sh')" ]; then
         continue
     fi
     if [ -z "$(echo $i | grep "build.rs")" ]; then
-        [ -f "$i" ] || continue
         echo  "#[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;" >> $i
     fi
